49133825e4068d5fa6c1fc889103e2425d4faf4b
ccrawford4 <calumcrawford9@gmail.com>
2025-03-01 08:11:37 -0800

    try tagging differently

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Check if all required arguments are provided
 if [ "$#" -lt 10 ]; then
-    echo "Usage: $0 <PRIVATE_KEY> <USER_NAME> <HOST> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_SESSION_TOKEN> <AWS_REGION> <ECR_REPOSITORY_URI> <DB_DSN> <RELEASE>"
+    echo "Usage: $0 <PRIVATE_KEY> <USER_NAME> <HOST> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_SESSION_TOKEN> <AWS_REGION> <ECR_REPOSITORY_URI> <DB_DSN>"
     exit 1
 fi
 
@@ -16,7 +16,6 @@ AWS_SESSION_TOKEN="$6"
 AWS_REGION="$7"
 ECR_REPOSITORY_URI="$8"
 DB_DSN="$9"
-RELEASE="$10"
 
 echo "$HOST"
 echo "$AWS_REGION"
@@ -26,7 +25,7 @@ echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 
 # Execute remote commands, passing environment variables explicitly
 ssh -o StrictHostKeyChecking=no -i private_key ${USER_NAME}@${HOST} bash -s \
-  -- "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$AWS_SESSION_TOKEN" "$AWS_REGION" "$ECR_REPOSITORY_URI" "$DB_DSN" "$RELEASE" << 'EOF'
+  -- "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$AWS_SESSION_TOKEN" "$AWS_REGION" "$ECR_REPOSITORY_URI" "$DB_DSN" << 'EOF'
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_SESSION_TOKEN=$3
@@ -38,8 +37,8 @@ echo "DB_DSN=$(echo "$6" | sed 's/\\//g')" > .env
 docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) $ECR_REPOSITORY_URI
 docker rm -v -f $(docker ps -aq)
 docker pull $ECR_REPOSITORY_URI:frontend
-docker run -p "3000:80" --env-file .env -d $ECR_REPOSITORY_URI:frontend:$RELEASE
-docker pull $ECR_REPOSITORY_URI:backend
-docker run -p "8080:8080" --env-file .env -d $ECR_REPOSITORY_URI:backend:$RELEASE
+docker run -p "3000:80" --env-file .env -d $ECR_REPOSITORY_URI/frontend:latest
+docker pull $ECR_REPOSITORY_URI/backend:latest
+docker run -p "8080:8080" --env-file .env -d $ECR_REPOSITORY_URI/backend:latest
 sudo systemctl is-active --quiet nginx || sudo systemctl start nginx
 EOF
